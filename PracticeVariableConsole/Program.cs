fb490c39464dc9e045fbd97783daf8b8f1f12c12
LekiaH <lekia.hood@gmail.com>
2022-03-01 15:11:36 -0500

    PracticeVariable

---
+++ PracticeVariableConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PracticeVariableConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string catName = "Ash";
+            int age = 7;
+            char myCharacter = 'L';
+            bool isOld = true;
+            double food = (double)1.5m;
+            decimal price = 4.23m;
+
+            Console.WriteLine($"My cat's name is {catName}. He is {age} years old. He {isOld} for his age. His middle initial is {myCharacter}. He eats {food} cans a day. I spend around {price} a week on canned food.");
+            
+
+
+
+      
+
+
+        }
+    }
+}
